6ad0425c7955db4997ba1cc15d6d1ec4cbba181c
finbmt <47vn.fin@gmail.com>
2015-10-17 23:36:19 +0700

    commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * @author TinhNQ *
+     * @category guide *
+     **/
+    $message = "xin chao moi nguoi 11111";
+    //echo zzzzz;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <?php echo $message;?>
+    </body>
+</html>
